2737cab824c76582b336d37544923ce43ecc82d4
Nem-Ajvaz <45509371+Nem-Ajvaz@users.noreply.github.com>
2021-08-09 23:00:42 +1000

    First Commit

---
+++ index.js
@@ -0,0 +1,123 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const renderLicense = require("./utils/generateMarkdown.js");
+const writeReadMe = util.promisify(fs.writeFile);
+input = (input) => {
+  if (input) {
+    return true;
+  }
+  return "Please ensure your input is entered.";
+};
+
+// TODO: Create an array of questions for user input
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Please enter your project title.",
+      validate: input,
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "What was the motivation behind this project?",
+      validate: input,
+    },
+    {
+      type: "input",
+      name: "problem",
+      message: "What problem are you trying to solve?",
+      validate: input,
+    },
+    {
+      type: "input",
+      name: "install",
+      message: "What are the steps required to install your project?",
+      validate: input,
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Please provide some instructions and examples of use.",
+      validate: input,
+    },
+    {
+      type: "input",
+      name: "githubUsername",
+      message: "What is your GitHub username?",
+      validate: input,
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter your email address.",
+      validate: input,
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: "How can people contribute",
+      validate: input,
+    },
+    {
+      type: "checkbox",
+      name: "license",
+      choices: [
+        "MIT",
+        "GNU_GPLv3",
+        "Apache_v2",
+        "BSD_2_Clause",
+        "MPL_2",
+        "None",
+      ],
+      default: ["MIT"],
+    },
+  ]);
+};
+
+// TODO: Create a function to write README file
+
+const generateREADME = (response) => {
+  const license = renderLicense(response.license[0]);
+
+  return `
+# ${response.title}
+${license}
+    
+## Description
+The motivation behind this project was to: ${response.description}
+This project will solve the following problem:  ${response.problem}
+## License
+${response.license} License 
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [License](#license)
+- [Questions](#questions)
+## Installation
+${response.install}
+## Usage
+${response.usage}
+## Contribution
+Against my better judgement I am not taking any contributions right now.
+${response.contribution}
+## Questions 
+For any further questions please reach out to my email :Email: ${response.email} \
+or message me on GitHub: [${response.github}](https://github.com/${response.github})
+`;
+};
+
+// TODO: Create a function to initialize app
+const init = () => {
+  questions()
+    .then((response) => writeReadMe("README.md", generateREADME(response)))
+    .then(() => console.log("Your README.md has been successfully generated."))
+    .catch((err) => console.error(err));
+};
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,33 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicense(license) {
+  switch (license) {
+    case "MIT": {
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    }
+    case "GNU_GPLv3": {
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    }
+    case "Apache_v2": {
+      return "[![License Apache v2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    }
+    case "BSD_2_Clause": {
+      return "[![License: BSD 2 Clause](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)";
+    }
+    case "MPL_2": {
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+    }
+    default: {
+      return "";
+    }
+  }
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = renderLicense;
